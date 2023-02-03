1dcf37f5c67320c29a41697ceed507867fb92b47
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-03 17:49:13 +0100

    Create Calculator_itf.java

---
+++ src/_pkg_echo/Calculator_itf.java
@@ -0,0 +1,8 @@
+package _pkg_echo;
+
+interface Calculator_itf {
+	public int plus(int a, int b);
+	public int minus (int a, int b);
+	public int divide(int a, int b);
+	public int multiply(int a, int b);
+}
