270aeec0d598479857d838f0aaeafa6e9ffcc054
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-04 08:24:47 +0100

    Added Person and Calculator interface

---
+++ src/_pkg_echo/Registry_itf.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package _pkg_echo;
+
+interface Registry_itf {
+	public void add(Person p);
+	public String getPhone(String name);
+	public Iterable<Person> getAll();
+	public Person search(String name);
+}
