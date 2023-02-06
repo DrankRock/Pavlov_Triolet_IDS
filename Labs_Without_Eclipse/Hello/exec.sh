f03fcc0940a820ac26aea010602c2225b34b1528
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-06 17:41:07 +0100

    feat : Script to auto launch server on new tab

---
+++ Labs_Without_Eclipse/Hello/exec.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Automatically launch the server in a new terminal
+echo "remove *.class"
+rm ./*.class
+echo "compile *"
+javac *.java
+# Get the process ID of rmiregistry
+pid=$(pidof rmiregistry)
+echo "kill rmiregistry if already running"
+# Check if the process ID is not empty
+if [ -n "$pid" ]
+then
+  # Kill the rmiregistry process
+  kill $pid > /dev/null
+else
+  # Do nothing if rmiregistry is not active
+  echo "" > /dev/null
+fi # by ChatGPT lol
+echo "open new terminal"
+xdotool key "ctrl+shift+t"; xdotool type "rmiregistry &"; xdotool key Return; xdotool type "java ./HelloServer.java"; xdotool key Return; xdotool key "alt+shift+1";
+echo "server successfully running."
+echo "wait for server to start"
+sleep 1
+echo "test HelloClient"
+expect=$(java HelloClient.java localhost)
+if [ "$expect" == "Hello world !" ]; then
+  echo "HelloClient works !"
+else :
+  echo "Something went wrong, sorry :c"
+fi
