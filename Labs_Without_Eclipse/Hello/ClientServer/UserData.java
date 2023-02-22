f7af9143fe1468dd786f0d9567e922477cba3322
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-22 16:28:01 +0100

    UserData class to manipulate content of file, but kinda useless

---
+++ Labs_Without_Eclipse/Hello/ClientServer/UserData.java
@@ -0,0 +1,30 @@
+public class UserData {
+    private String user;
+    private String salt;
+    private String password;
+    public UserData(String user, String salt, String password){
+        this.user = user;
+        this.salt = salt;
+        this.password = password;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getSalt() {
+        return salt;
+    }
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+
+}
