019c7c6fcebdf6e6e6aa62555cdb307bd52640ba
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-23 13:23:37 +0100

    add gui and file functions

--- Labs_Without_Eclipse/Hello/ClientServer/Server.java
+++ Labs_Without_Eclipse/Hello/ClientServer/Server.java
@@ -10,4 +10,7 @@ public interface Server extends Remote {
     public int nextID()  throws RemoteException;
     public int connect(String user, String pass, ClientMessagesInterface cmi) throws RemoteException;
     public void disconnect(Info_itf_Impl infos) throws RemoteException;
+    public void startGUI(String identifier) throws RemoteException, NotBoundException;
+    public void loadUsers(String s) throws RemoteException;
+    public void loadHistory(String s) throws RemoteException;
 }  
