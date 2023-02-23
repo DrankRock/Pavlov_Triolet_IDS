252e3fd9c03e24300842066dbaecefd546e2da8b
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-23 13:23:52 +0100

    add and improve server view

--- Labs_Without_Eclipse/Hello/ClientServer/ServerImpl.java
+++ Labs_Without_Eclipse/Hello/ClientServer/ServerImpl.java
@@ -1,8 +1,7 @@
-// Importing the RMI package.
-import java.io.File;
 import java.rmi.*;
 import java.security.SecureRandom;
-import java.util.Random;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -12,29 +11,36 @@ import java.util.HashMap;
  */
 public  class ServerImpl implements Server {
 
-	private String message;
 	private int lastGivenID;
 	private HashMap<String, String> userToPass;
 	private HashMap<String, Integer> userToID;
 	private HashMap<String, ClientMessagesInterface> userToClientStub;
+	private ArrayList<String> chatHistory;
 	public SecureRandom r = new SecureRandom();
 	FileLoader history;
 	FileLoader userData;
+
+	ServerApp gui;
+
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss:SSS:AAAA");
+
+	private String callerServerIDHash;
  
-	public ServerImpl(String s, int i) {
-		message = s ;
+	public ServerImpl(String s, int i){
+		callerServerIDHash = s ;
 		lastGivenID = 0;
 		userToPass = new HashMap<>();
 		userToID = new HashMap<>();
 		userToClientStub = new HashMap<>();
 		history = new FileLoader(".history");
 		userData = new FileLoader(".userdata");
-		try {
-			loadUsers();
-			loadHistory();
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+	}
+
+	public void startGUI(String s) throws RemoteException, NotBoundException{
+		if (s.equals(callerServerIDHash)){
+			gui = new ServerApp();
+			gui.run();
 		}
 	}
 
@@ -45,21 +51,21 @@ public  class ServerImpl implements Server {
 	// returns the id of the user
 	public int connect(String user, String pass, ClientMessagesInterface cmi) throws RemoteException{
 		int ret = -1;
-		Tools.dprint("[CONNECT] - "+user+" : "+pass);
+		log("[CONNECT] ("+user+") - "+user+" : "+pass);
 
 		if (userToPass.containsKey(user)){
-			Tools.dprint("[CONNECT] - user exists");
-			Tools.dprint("[CONNECT] - userToPass(user) = "+userToPass.get(user));
+			log("[CONNECT] ("+user+") - user exists");
+			log("[CONNECT] ("+user+") - userToPass(user) = "+userToPass.get(user));
 		
 			if (arePasswordsEqual(userToPass.get(user), pass)){
-				Tools.dprint("[CONNECT] - "+user+" successfully connected.");
+				log("[CONNECT] ("+user+") - "+user+" successfully connected.");
 				ret = userToID.get(user);
 				cmi.displayMessage("Welcome back, "+user+".");
 			} else {
-				Tools.dprint("[CONNECT] - incorrect password");
+				log("[CONNECT] ("+user+") - incorrect password");
 			}
 		} else {
-			Tools.dprint("[CONNECT] - create user");
+			log("[CONNECT] ("+user+") - create user");
 			// create user
 			String salt = Security.bytesToHex(Security.getSalt());
 			String hashedPwd = Security.encode(pass, salt);
@@ -72,8 +78,13 @@ public  class ServerImpl implements Server {
 		}
 		if (ret != -1 ){
 			userToClientStub.put(user, cmi);
+			giveHistoryToUser(cmi);
 		}
 		return ret;
+	}
+
+	private void giveHistoryToUser(ClientMessagesInterface cmi){
+
 	}
 
 	public boolean arePasswordsEqual(String saltAndPass, String passwd){
@@ -84,37 +95,47 @@ public  class ServerImpl implements Server {
 
 	public void message(String user, String s) throws RemoteException{
 		// broadcast message
-		Tools.dprint("Message sent : "+s);
+		log("[INFO] - Message recieved from <"+user+">");
 
 		for (ClientMessagesInterface client : userToClientStub.values()) {
 			client.displayMessage(user, s);
-			// ...
 		}
 		
 
 	}
 
 	public void disconnect(Info_itf_Impl infos) throws RemoteException{
-		Tools.dprint("Removing "+infos.name+" from active users");
+		log("[DISCONNECT] - Removing "+infos.name+" from active users");
 		userToClientStub.remove(infos.getName());
 	}
 
-	private void loadUsers() throws RemoteException{
-		ArrayList<String> lines = userData.readLines();
-		for (String line : lines){
-			try {
-				UserData current = Security.stringToUserdata(line);
-				userToPass.put(current.getUser(), current.getSalt()+":"+current.getPassword());
-				userToID.put(current.getUser(), this.nextID());
-			} catch (NullPointerException npe){
-				Tools.dprint("line :'"+line+"'\nContains no user:salt:password");
+	public void loadUsers(String s) throws RemoteException{
+		if (s.equals(callerServerIDHash)){
+			ArrayList<String> lines = userData.readLines();
+			log("[SETUP] - Loading Users : ");
+			for (String line : lines){
+				try {
+					UserData current = Security.stringToUserdata(line);
+					userToPass.put(current.getUser(), current.getSalt()+":"+current.getPassword());
+					userToID.put(current.getUser(), this.nextID());
+					log("[SETUP] | - Loaded : "+current.getUser());
+				} catch (NullPointerException npe){
+					log("[SETUP] | - line :'"+line+"' Contains no user:salt:password");
+				}
 			}
 		}
 	}
 
-	private void loadHistory(){
-		ArrayList<String> lines = history.readLines();
+	public void loadHistory(String s){
+		if (s.equals(callerServerIDHash)){
+			chatHistory = history.readLines();
+			log("[SETUP] - HISTORY LOADED ("+chatHistory.size()+" lines)");
+		}
 
+	}
+
+	private void log(String s){
+		gui.log("("+dtf.format(LocalTime.now())+") "+s);
 	}
 
 }
