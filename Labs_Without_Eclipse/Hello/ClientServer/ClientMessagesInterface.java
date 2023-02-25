259d9f8190ae635588b86d0593a13f6bc314e527
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-25 21:18:50 +0100

    doc : commented

--- Labs_Without_Eclipse/Hello/ClientServer/ClientMessagesInterface.java
+++ Labs_Without_Eclipse/Hello/ClientServer/ClientMessagesInterface.java
@@ -1,16 +1,15 @@
-import java.lang.ProcessHandle.Info;
 import java.rmi.*;
 import java.util.ArrayList;
 
 /**
- * RMI Interface "Hello"
- * @version 0.1
- * 
+ * Interface of the ClientMessage, that is used by the server to send message to
+ * the client.
  */
 public interface ClientMessagesInterface extends Remote {
 	public void displayMessage(String s) throws RemoteException;
 	public void displayMessage(String user, String s) throws RemoteException;
 	public void displayMessage(ArrayList<String> ss) throws RemoteException;
+	public void displayMessageGreen(String s) throws RemoteException;
 	public void run(Info_itf_Impl inf) throws RemoteException, NotBoundException;
 	public void closingServer() throws RemoteException;
 }  
