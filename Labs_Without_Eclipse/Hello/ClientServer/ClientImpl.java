d0470eb923cb429361afcb1bcdaff19143af588e
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-25 18:57:03 +0100

    added a lot, fixed issues

--- Labs_Without_Eclipse/Hello/ClientServer/ClientImpl.java
+++ Labs_Without_Eclipse/Hello/ClientServer/ClientImpl.java
@@ -1,101 +1,131 @@
 import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.Scanner;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-/**
- * Class HelloClient, connects to the server and executes "sayHello", then returns 
- * 
- */
 public class ClientImpl {
 	private Server h;
 	private int myID;
+
 	Info_itf_Impl infos;
 	Thread listener;
 	ClientMessages messaging;
 
-	public ClientImpl(String user, String pass, String host, Server h) throws RemoteException, NotBoundException{
-		Tools.dprint("Create clientimpl");
+	public ClientImpl(String host, Server h) throws RemoteException, NotBoundException{
 		this.h = h;
-		this.messaging = new ClientMessages(user);
-		ClientMessagesInterface messaging_stub = (ClientMessagesInterface) UnicastRemoteObject.exportObject(messaging, 0);	
-		myID = this.h.connect(user, pass, messaging_stub);
-		if (myID == -1){
-			System.out.println("Incorrect Password .. ");
+		this.infos = new Info_itf_Impl("", "");
+	}
+
+	public void connectionWithServer(String user, String pass) throws RemoteException, NotBoundException{
+		messaging = new ClientMessages(user);
+		ClientMessagesInterface messaging_stub = (ClientMessagesInterface) UnicastRemoteObject.exportObject(messaging, 0);
+		Tools.dprint("Launched messages stub");
+		String connectionID = h.connect(user, pass, messaging_stub) ;
+		if( connectionID == null){
+			System.out.println("Incorrect Password.");
 			System.exit(0);
+		} else {
+			infos = new Info_itf_Impl(user, connectionID);
 		}
-		Tools.dprint("Connected. Given id : "+myID);
-		this.infos = new Info_itf_Impl(user, myID);
-		Tools.dprint("Given infos : "+this.infos);
-		
-		//messaging.displayMessage("Initializing user : "+user);
+		messaging_stub.run(infos);
+		h.notifyOfActiveGUI(infos);
 	}
-
+	/**
+	 * Send message to server
+	 * @param s String containing the message
+	 * @throws RemoteException RMI connection issues
+	 */
 	public void sendMessage(String s) throws RemoteException{
-		// send message to server
-		Tools.dprint("[INFO] - "+infos.name+" sending \""+s+"\"");
-		h.message(infos.getName(), s);
+		Tools.dprint("[INFO] - "+infos.getName()+" sending \""+s+"\"");
+		h.message(infos, s);
 	}
 
-	public void recieveMessage(String s) {
-		System.out.println("[MSG] : "+s);
-	}
-
+	/**
+	 * Disconnect from the server (not currently used by this class)
+	 * @throws RemoteException
+	 */
 	public void disconnect() throws RemoteException {
 		h.disconnect(infos);
 	}
 
 	public String toString(){
-		return infos.toString()+", "+myID;
+		return infos.toString();
 	}
+	/**
+	 * Disconnects then quits
+	 * @throws RemoteException RMI Connection issues
+	 */
 	public void quit() throws RemoteException{
 		this.disconnect();
-		//this.listener.interrupt();
 		System.exit(0);
 	}
-	public void launchListener(){
-		/*
-		listener = new Thread(){
-			public void run(){
-				while(!Thread.interrupted()){
-					
+
+	/**
+	 * First step of the execution : the connection page
+	 */
+	public void run(){
+		// open connexion page
+		Tools.dprint("Call run");
+		ConnexionClient cc = new ConnexionClient();
+		cc.frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				String username = cc.getUsername();
+				String password = cc.getPassword();
+				try {
+					System.out.println(username+", "+password);
+					continueRun(username, password);
+				} catch (RemoteException | NotBoundException e1) {
+					e1.printStackTrace();
 				}
 			}
-		  };
-		
-		  listener.start();*/
+		});
 	}
 
-	public void run() throws RemoteException{
-		Scanner scanner = new Scanner(System.in);
-		String s;
-		while (scanner.hasNextLine()){
-			s = scanner.nextLine();
-			if (s.equals("quit")){
-				scanner.close();
-				this.quit();
-			} else {
-				sendMessage(s);
-			}
+	/**
+	 * Second step of the execution : connect to the server
+	 * @param username
+	 * @param password
+	 * @throws RemoteException RMI Connection issues
+	 * @throws NotBoundException
+	 */
+	public void continueRun(String username, String password) throws RemoteException, NotBoundException{
+		if (username == ""){
+			throw new IllegalArgumentException("The entered Username is not valid");
 		}
-		scanner.close();
+		System.out.println("Continue with "+username+", "+password);
+		this.connectionWithServer(username, password);
+	}
+
+	public Info_itf_Impl getInfos(){
+		return this.infos;
 	}
 
 	public static void main(String [] args) throws RemoteException, NotBoundException {
-		if (args.length != 3) {
-			System.out.println("Usage: java HelloClient <rmiregistry host ex: localhost> <username> <password>");
+		if (args.length != 1) {
+			System.out.println("Usage: java HelloClient <rmiregistry host ex: localhost>");
 			return;
 		}
 		// Get remote object reference
 		Registry registry = LocateRegistry.getRegistry(args[0]); // get the registry of the host given in argument
 		// Here, we will always use "localhost"
 		Server h = (Server) registry.lookup("RunningServerPT1"); //get the server running on the RMI
-		ClientImpl hc = new ClientImpl(args[1], args[2], args[0], h);
 
+		ClientImpl hc = new ClientImpl(args[0], h);
+		hc.run();
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+					hc.disconnect();
+				} catch (RemoteException e) {
+					e.printStackTrace();
+				}
+            }
+        });
 		//ClientImpl cc = (ClientImpl) UnicastRemoteObject.exportObject(hc, 0);	
 
-		hc.run();
-		System.exit(1);
+		//System.exit(1);
 		//hc.run(args);
 	}
 }
