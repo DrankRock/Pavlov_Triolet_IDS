e48630275b9a3ff7524f1b09bb8f490091041381
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-25 22:21:32 +0100

    doc : commented

--- Labs_Without_Eclipse/Hello/ClientServer/Info_itf.java
+++ Labs_Without_Eclipse/Hello/ClientServer/Info_itf.java
@@ -1,6 +1,8 @@
-
 import java.rmi.*;
 
+/**
+ * Interface of the info class
+ */
 public interface Info_itf {
 		public String getName() throws RemoteException;
 		public String getID() throws RemoteException;
