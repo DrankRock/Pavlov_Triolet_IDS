b44af8410760671b13717f0508df1bbdd3700d5f
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-25 23:46:13 +0100

    commented + randomString

--- Labs_Without_Eclipse/Hello/ClientServer/Tools.java
+++ Labs_Without_Eclipse/Hello/ClientServer/Tools.java
@@ -1,8 +1,30 @@
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+
+/**
+ * Tools set used around
+ */
 public class Tools {
-    public static boolean DEBUG = true;
+    public static boolean DEBUG = false;
+    /**
+     * Print only if DEBUG is true
+     * @param s String to print
+     */
     public static void dprint(String s){
         if (DEBUG){
             System.out.println(s);
         }
+    }
+
+    /**
+     * Return a random String
+     * It's a pretty unique String. Not absolutely unique though. This can't be achieved.
+     * @param size of the array of bytes
+     * @return random bytes array as a String
+     */
+    public static String randomString(int size){
+        byte[] array = new byte[size];
+        new SecureRandom().nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
     }
 }
