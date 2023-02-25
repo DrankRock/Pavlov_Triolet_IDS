fa8fda37abdcab0065882130f568da3d2ca8ca9e
DrankRock <32172257+DrankRock@users.noreply.github.com>
2023-02-25 22:21:08 +0100

    doc :  commented

--- Labs_Without_Eclipse/Hello/ClientServer/Info_itf_Impl.java
+++ Labs_Without_Eclipse/Hello/ClientServer/Info_itf_Impl.java
@@ -1,21 +1,38 @@
-
 import java.io.Serializable;
 import java.rmi.*;
 
+/**
+ * Class containing two strings, to send infos about the client easily.
+ */
 public class Info_itf_Impl implements Info_itf, Serializable{
 	private String name;
 	private String ID;
+	/**
+	 * Constructor
+	 * @param s username
+	 * @param id identifier hash
+	 * @throws RemoteException RMI Connection issues
+	 */
 	public Info_itf_Impl(String s, String id) throws RemoteException{ 
 		name = s;
 		ID = id;
 	}
+	/**
+	 * getter of username
+	 */
 	public String getName() throws RemoteException{
 		return name;
 	}
+	/**
+	 * getter of identifier
+	 */
 	public String getID() throws RemoteException{
 		return ID;
 	}
+	/**
+	 * return string of the infos.
+	 */
 	public String toString(){
-		return name+" - "+ID;
+		return "(Infos_itf_Impl) "+name+", "+ID;
 	}
 }
